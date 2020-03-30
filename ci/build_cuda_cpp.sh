ca933e26b329ecc034a0300cef83b6fb79f6fe88
Geza Velkey <evelkey@gmail.com>
2020-03-30 13:44:02 +0200

    Fixed TRT build

--- ci/build_cuda_cpp.sh
+++ ci/build_cuda_cpp.sh
@@ -3,6 +3,7 @@ repo_dir="${ci_dir}/.."
 
 base_image=$1
 target_image=$2
+cuda_version=$3
 
 if [[ -z "${base_image}" ]]; then
     base_image=" xmindai/cuda-cudnn-opengl:ubuntu18.04-10.0-cudnn7"  # xmindai/cuda-cudnn-opengl:ubuntu18.04-10.0-cudnn7
@@ -10,6 +11,11 @@ fi
 
 if [[ -z "${target_image}" ]]; then
     target_image="xmindai/cuda-cpp:ubuntu18.04-10.0-cudnn7"
+fi
+
+if [[ -z "${cuda_version}" ]]; then
+    echo "No cuda version given" 1>&2
+    exit 1
 fi
 
 set -e
@@ -21,7 +27,7 @@ cd "${repo_dir}/user-layer"
 ./build.sh temp-dev-img temp-dev-user-img
 
 cd "${repo_dir}/cuda-cpp-development"
-./build.sh temp-dev-user-img ${target_image}
+./build.sh temp-dev-user-img ${target_image} ${cuda_version}
 
 # cleanup
 docker rmi temp-dev-img temp-dev-user-img
